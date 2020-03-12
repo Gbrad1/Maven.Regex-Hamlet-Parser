915f3a763c02b4cff00b8602c6bbb20e0fdee600
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-03-11 21:01:11 -0400

    saving progress

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -8,6 +10,8 @@ import java.util.Scanner;
 public class HamletParser {
 
     private String hamletData;
+    private String leonRegex = "[Hh][Aa][Mm][Ll][Ee][Tt]'?s?";
+    private String tariqRegex = "[Hh][Oo][Rr][Aa][Tt][Ii][Oo]'?s?";
 
     public HamletParser(){
         this.hamletData = loadFile();
@@ -36,4 +40,51 @@ public class HamletParser {
         return hamletData;
     }
 
+    public void printData() {
+        System.out.println(hamletData);
+    }
+
+    public void changeHamletToLeon() {
+        Pattern pattern = Pattern.compile(leonRegex);
+        Matcher matcher = pattern.matcher(hamletData);
+        String results = matcher.replaceAll("Leon");
+        hamletData = results;
+    }
+
+    public void changeHoratioToTariq() {
+        Pattern pattern = Pattern.compile(tariqRegex);
+        Matcher matcher = pattern.matcher(hamletData);
+        String results = matcher.replaceAll("Tariq");
+        hamletData = results;
+    }
+
+    public String getData() {
+        return hamletData;
+    }
+
+    public String changeBothNames() {
+        changeHamletToLeon();
+        changeHoratioToTariq();
+        return getData();
+    }
+
+    public int getNumberOfHamlets() {
+        Pattern pattern = Pattern.compile(leonRegex);
+        Matcher matcher = pattern.matcher(hamletData);
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+        return count;
+    }
+
+    public int getNumberOfHoratio() {
+        Pattern pattern = Pattern.compile(tariqRegex);
+        Matcher matcher = pattern.matcher(hamletData);
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+        return count;
+    }
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,11 +1,17 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
 public class HamletParserTest {
     private String hamletText;
     private HamletParser hamletParser;
+    private String leonRegex = "[^e][Ll][Ee][Oo][Nn]'?s?";
+    private String tariqRegex = "[Tt][Aa][Rr][Ii][Qq]'?s?";
 
     @Before
     public void setUp() {
@@ -15,17 +21,54 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        hamletParser.changeHamletToLeon();
+        Pattern pattern = Pattern.compile(leonRegex);
+        Matcher matcher = pattern.matcher(hamletParser.getHamletData());
+
+        int expected = 472;
+
+        int actual = 0;
+        while (matcher.find()) {
+            actual++;
+        }
+
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        hamletParser.changeHoratioToTariq();
+        Pattern pattern = Pattern.compile(tariqRegex);
+        Matcher matcher = pattern.matcher(hamletParser.getHamletData());
+
+        int expected = 158;
+
+        int actual = 0;
+        while (matcher.find()) {
+            actual++;
+        }
+
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHoratio() {
+        int actual = hamletParser.getNumberOfHoratio();
+        int expected = 158;
+
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHamlet() {
+        int actual = hamletParser.getNumberOfHamlets();
+        int expected = 472;
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void printTest() {
+        hamletParser.printData();
     }
 }
